e51acaba2c88f6c7bc3377ee6dbe24c569517763
Gustavo Guidotti <gustavogguidotti@hotmail.com>
2026-02-19 04:25:13 -0300

    add try/catch and async/await

--- js/utils.js
+++ js/utils.js
@@ -2,53 +2,69 @@ export const getEl = el => document.getElementById(el);
 export const form = document.querySelector('form');
 
 const listaUsuarios = getEl('listaUsuarios');
+const URL = `https://crudcrud.com/api/ad10a0476dea44f09c76cd273d769429/usuarios`;
 
 // Helper function para cadastrar usuario no CrudCrud
-export const cadastrarUsuario = usuario => {
-    fetch(
-        'https://crudcrud.com/api/2f87b6cf02024ea5a4cac74258c272eb/usuarios',
-        {
+export const cadastrarUsuario = async usuario => {
+    try {
+        const res = await fetch(URL, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(usuario),
-        },
-    ).catch(error => {
-        console.error('Erro ao cadastrar usuario.', error);
-    });
+        });
+
+        if (!res.ok) {
+            throw new Error('Erro ao cadastrar usuario.');
+        }
+        return await res.json();
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 // Deleta usuario do CrudCrud e remove elemento HTML correspondente
-const excluir = id => {
-    fetch(
-        `https://crudcrud.com/api/2f87b6cf02024ea5a4cac74258c272eb/usuarios/${id}`,
-        {
+const excluir = async id => {
+    getEl(id)?.remove();
+    try {
+        const res = await fetch(`${URL}/${id}`, {
             method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        },
-    ).then(() => {
-        getEl(id).remove();
-    });
+        });
+        if (!res.ok) {
+            throw new Error('Erro ao excluir usuario');
+        }
+    } catch (error) {
+        console.error(error);
+    }
 };
 
-export const listarUsuarios = () => {
-    fetch('https://crudcrud.com/api/2f87b6cf02024ea5a4cac74258c272eb/usuarios')
-        .then(res => res.json())
-        .then(usuarios => {
-            listaUsuarios.innerHTML = '';
-            usuarios.forEach(usuario => {
-                const item = document.createElement('li');
-                item.id = usuario._id;
-                item.innerHTML = `Nome: ${usuario.nome}<br>
+export const listarUsuarios = async () => {
+    try {
+        const res = await fetch(URL);
+        if (!res.ok) {
+            throw new Error('Erro ao listar usuarios.');
+        }
+        const usuarios = await res.json();
+
+        listaUsuarios.innerHTML = '';
+
+        usuarios.forEach(usuario => {
+            const item = document.createElement('li');
+            item.id = usuario._id;
+
+            item.innerHTML = `
+                Nome: ${usuario.nome}<br>
                 E-mail: ${usuario.email}
-                <button id="button-${usuario._id}">X</button>`;
-                listaUsuarios.appendChild(item);
-                getEl(`button-${usuario._id}`).addEventListener('click', () =>
-                    excluir(usuario._id),
-                );
-            });
+                <button>X</button>`;
+
+            listaUsuarios.appendChild(item);
+
+            item.querySelector('button').addEventListener('click', () =>
+                excluir(usuario._id),
+            );
         });
+    } catch (error) {
+        console.error(error);
+    }
 };
